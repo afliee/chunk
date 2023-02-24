96aae3ca4735c3796296f6a99a3234d172387847
Kunzzz <quoctruong310803@gmail.com>
2023-02-24 19:10:58 +0700

    update env

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import express from 'express';
 import morgan from 'morgan';
 import viewEngine from './config/viewEngine';
@@ -12,6 +13,8 @@ app.use(express.json());
 viewEngine(app);
 route(app);
 app.use(morgan('combined'));
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
